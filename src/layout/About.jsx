bce4e8181e417b58bd7640bd9cab87cbdbde9819
Romina Osorio <r.osorio.marmolejo@gmail.com>
2025-03-21 18:44:22 -0300

    Arreglé resposividad en About.jsx para pantallas md

--- src/layout/About.jsx
+++ src/layout/About.jsx
@@ -19,13 +19,13 @@ const About = () => {
   return (
     <>
       <section id='about' className={`${darkMode ? 'dark bg-black' : 'light bg-transparent'} w-full m-auto lg:px-40 px-10 py-20 grid lg:grid-cols-2 grid-cols-1 justify-center items-center gap-10`}>
-        <div>
-          <img data-aos='zoom-in' src={aboutimg} alt='about image' className='rounded-2xl lg:w-[500px] lg:h-[600px]' />
+        <div className='flex justify-center'>
+          <img data-aos='zoom-in' src={aboutimg} alt='about image' className='rounded-2xl w-full max-w-[400px] md:max-w-[500px] md:h-[450px] lg:h-[600px] object-cover' />
         </div>
 
         <div className='flex flex-col justify-center items-start gap-8'>
           <h2 data-aos='zoom-in' className='text-red-500 dark:text-white uppercase font-semibold'>Quienes somos</h2>
-          <h3 data-aos='zoom-in' data-aos-delay='200' className='text-black text-[35px] font-semibold leading-10 dark:text-white'>
+          <h3 data-aos='zoom-in' data-aos-delay='200' className='text-black text-[28px] md:text-[32px] lg:text-[35px] font-semibold leading-10 dark:text-white'>
             Ofrecemos asesoría para la compra y venta de propiedades.
           </h3>
           <p data-aos='zoom-in' data-aos-delay='400' className='text-xl text-gray-600 dark:text-white text-justify'>
