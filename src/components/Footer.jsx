545fa31e2256e725bd7fc7ae5032f8cc7e137503
Romina Osorio <r.osorio.marmolejo@gmail.com>
2025-03-25 18:02:38 -0300

    Agregué componente  Header.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,8 +17,8 @@ const Footer = () => {
       delay: 100
     })
   }, [])
-
   const { darkMode, toggleDarkMode } = useDarkMode()
+
   return (
     <>
       <footer className={`${darkMode ? 'dark: bg-black' : 'light bg-gray-800'} w-full m-auto lg:px-20 px-10 py-20 grid lg:grid-cols-3 grid-cols-1 justify-center items-start lg:gap-20 gap-10`}>
